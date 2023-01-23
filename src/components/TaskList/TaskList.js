93de9feba5a1996bc09c3399a377217471473a49
Sergey <smisyuk@gmail.com>
2023-01-23 23:35:41 +0200

    done  example from lesson

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,14 +1,37 @@
+// Імпортуємо хук
+import { useSelector } from "react-redux";
 import { Task } from "components/Task/Task";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
 import css from "./TaskList.module.css";
 
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+
 export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(state => state.tasks);
+  // Отримуємо значення фільтра із стану Redux
+  const statusFilter = useSelector(state => state.filters.status);
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  
   return (
-    <ul className={css.list}>
-      {[].map(task => (
-        <li className={css.listItem} key={task.id}>
-          <Task task={task} />
-        </li>
-      ))}
-    </ul>
+    <ul className={css.list}>     
+     {visibleTasks.map(task => (
+      <li className={css.listItem} key={task.id}>
+        <Task task={task} />
+      </li>
+    ))}    
+  </ul>
   );
 };
